a00001553301de213ae96c9414e3cba53c0c9686
nournsiri <147239490+nournsiri@users.noreply.github.com>
2023-11-17 09:54:22 +0100

    Add files via upload

--- produit.java
+++ produit.java
@@ -1,34 +1,67 @@
 import java.time.LocalDate;
 
 public class produit {
-    LocalDate date_exp;
-    int identifier;
-    String libelle;
-    String marque;
-    double prix;
+    private LocalDate dateExp;
+    private int identifier;
+    private String libelle;
+    private String marque;
+    private double prix;
 
     public produit() {
 
     }
 
-    public produit(int identifier, String libelle, String marque, double prix, LocalDate date_exp) {
+    public produit(int identifier, String libelle, String marque, double prix, LocalDate dateExp) {
         this.identifier = identifier;
         this.libelle = libelle;
         this.marque = marque;
         this.prix = prix;
-        this.date_exp = date_exp;
+        this.dateExp = dateExp;
     }
 
-    void affiche() {
-        System.out.println("le identifier " + identifier + " le libelle " + libelle + " le marque " + marque + " le prix " + prix + " la date est " + date_exp);
-        System.out.println(date_exp);
+    public void affiche() {
+        System.out.println("le identifier " + identifier + " le libelle " + libelle + " le marque " + marque + " le prix " + prix + " la date est " + dateExp);
+        System.out.println(dateExp);
     }
 
     public String toString() {
         return "identifier: " + this.identifier + ", libelle: " + this.libelle + ", marque: " + this.marque + ", prix: " + this.prix;
     }
 
-    public void setDate_ex(LocalDate date_exp) {
-        this.date_exp = date_exp;
+    public LocalDate getDateExp() {
+        return dateExp;
     }
+
+    public void setDateExp(LocalDate dateExp) {
+        this.dateExp = dateExp;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+    public int getidentifier() {
+        return this.identifier;
+    }
+    public void setidentifier(int identifier) {
+        this.identifier = identifier;
+    }
+
+
+    public boolean comparer1(produit prod) {
+        return this.libelle.equals(prod.getLibelle()) && this.getPrix() == prod.getPrix() && this.identifier == prod.getidentifier();
+    }
+
+    public boolean comparer2(produit prod1, produit prod2) {
+        return this.libelle.equals(prod1.getLibelle()) && this.getPrix() == prod1.getPrix() && this.identifier == prod1.getidentifier()
+                && this.libelle.equals(prod2.getLibelle()) && this.getPrix() == prod2.getPrix() && this.identifier == prod2.getidentifier();
+    }
+
+
 }
