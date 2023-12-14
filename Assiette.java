6572956975d75a76c3b943b4d430a24ffef23974
nournsiri <147239490+nournsiri@users.noreply.github.com>
2023-12-14 20:14:47 +0100

    Add files via upload

---
+++ Assiette.java
@@ -0,0 +1,9 @@
+public class Assiette  extends Ustensile{
+    public Assiette(int anneefabrication){
+        super((anneefabrication));
+    }
+
+    public double calculesurface() {
+        return 0;
+    }
+}
